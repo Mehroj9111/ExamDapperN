7a73dacd97b82dd78c1c73bd88208143e670acf1
Mehroj9111 <112384093+Mehroj9111@users.noreply.github.com>
2022-12-04 23:21:33 +0300

    Update EmployeeDto.cs

--- Domain/Dtos/EmployeeDto.cs
+++ Domain/Dtos/EmployeeDto.cs
@@ -17,3 +17,20 @@ public class EmployeeDto
      public List<string>? FileName { get; set; }
     public DateTime HireDate { get; set; }
 }
+ public int EmployeeId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public int PhoneNumber { get; set; }
+    public int DepartId { get; set; }
+    public int ManagerId { get; set; }
+    public int Commission { get; set; }
+    public int Salary { get; set; }
+    public int JobId { get; set; }
+     public List<string> File { get; set; }
+    public DateTime HireDate { get; set; }
+    public GetEmployeeDto()
+    {
+        File = new List<string>();
+    }
+}
